9b646da9de1c86ba83435261f029457e50164125
Jose Robinson <jrobinsonc@gmail.com>
2022-02-12 10:36:18 -0500

    chore: use vitejs, update eslint and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,157 @@
+const baseRules = {
+  'prettier/prettier': 'warn',
+
+  'default-case': 'warn',
+  'no-console': 'warn',
+  'no-debugger': 'warn',
+  'no-warning-comments': [
+    'warn',
+    { terms: ['@todo', '@fixme', 'TODO', 'FIXME'], location: 'anywhere' },
+  ],
+
+  'import/no-extraneous-dependencies': [
+    'error',
+    { devDependencies: ['**/*.test.ts', 'vite.config.js'] },
+  ],
+};
+
 module.exports = {
   root: true,
-
   env: {
-    es2021: true,
     browser: true,
-    jest: true,
+    node: true,
   },
-
   parserOptions: {
-    project: './tsconfig.json',
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
   },
-
-  plugins: ['@typescript-eslint', 'promise'],
-
+  plugins: ['jsdoc', 'prettier'],
   extends: [
-    'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-base',
+    'plugin:import/recommended',
     'plugin:promise/recommended',
     'plugin:prettier/recommended',
   ],
-
   rules: {
-    "prettier/prettier": "warn",
-  },
+    ...baseRules,
 
-  settings: {},
+    'no-unused-vars': 'warn',
+
+    'promise/catch-or-return': 'warn',
+  },
+  settings: {
+    jsdoc: {
+      mode: 'jsdoc',
+    },
+  },
+  overrides: [
+    {
+      files: '**/*.ts',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig-src.json',
+      },
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:promise/recommended',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        ...baseRules,
+
+        'promise/catch-or-return': 'off', // => @typescript-eslint/no-floating-promises
+
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        // In case "any" is required, add eslint-disable-next-line and a comment explaining why.
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/typedef': [
+          'warn',
+          {
+            arrayDestructuring: true,
+            arrowParameter: true,
+            memberVariableDeclaration: true,
+            objectDestructuring: true,
+            parameter: true,
+            propertyDeclaration: true,
+            variableDeclaration: true,
+            variableDeclarationIgnoreFunction: true,
+          },
+        ],
+      },
+      settings: {
+        jsdoc: {
+          mode: 'typescript',
+        },
+      },
+    },
+    {
+      files: 'src/**/*.ts',
+      rules: {
+        // JSDoc: https://github.com/gajus/eslint-plugin-jsdoc/tree/master/.README/rules
+        'jsdoc/require-description-complete-sentence': 'warn',
+        'jsdoc/require-jsdoc': [
+          'warn',
+          {
+            require: {
+              ClassDeclaration: true,
+              ArrowFunctionExpression: true,
+              ClassExpression: true,
+              FunctionDeclaration: true,
+              FunctionExpression: true,
+              MethodDefinition: true,
+            },
+          },
+        ],
+        'jsdoc/check-alignment': 'warn',
+        'jsdoc/check-indentation': 'warn',
+        'jsdoc/check-line-alignment': 'warn',
+        'jsdoc/check-param-names': 'warn',
+        'jsdoc/check-property-names': 'warn',
+        'jsdoc/check-syntax': 'warn',
+        'jsdoc/check-tag-names': 'warn',
+        'jsdoc/check-types': 'warn',
+        'jsdoc/check-values': 'warn',
+        'jsdoc/empty-tags': 'warn',
+        'jsdoc/multiline-blocks': 'warn',
+        'jsdoc/newline-after-description': 'warn',
+        'jsdoc/no-bad-blocks': 'warn',
+        'jsdoc/no-defaults': 'warn',
+        'jsdoc/no-multi-asterisks': 'warn',
+        'jsdoc/no-types': 'warn',
+        'jsdoc/no-undefined-types': 'warn',
+        'jsdoc/require-asterisk-prefix': 'warn',
+        'jsdoc/require-description': 'warn',
+        'jsdoc/require-hyphen-before-param-description': 'warn',
+        'jsdoc/require-param': 'warn',
+        'jsdoc/require-param-description': 'warn',
+        'jsdoc/require-param-name': 'warn',
+        'jsdoc/require-property': 'warn',
+        'jsdoc/require-property-description': 'warn',
+        'jsdoc/require-property-name': 'warn',
+        'jsdoc/require-returns': 'warn',
+        'jsdoc/require-returns-check': 'warn',
+        'jsdoc/require-returns-description': 'warn',
+        'jsdoc/require-throws': 'warn',
+      },
+    },
+    {
+      files: 'test/**/*.ts',
+      parserOptions: {
+        project: './tsconfig-tests.json',
+      },
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import banner from 'vite-plugin-banner';
+import pkg from './package.json';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'WeeFetch',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `${pkg.name}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['cross-fetch'],
+      output: {
+        globals: {},
+        exports: 'named',
+      },
+    },
+  },
+  plugins: [banner(`${pkg.name} v${pkg.version} - ${pkg.homepage}`)],
+});
